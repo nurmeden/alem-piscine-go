d86d27de2f447687f5f9e5c5ed10fe8015f2bc4b
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-03 16:12:52 +0000

    Update 'teacher.sh'

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,4 @@
-INTT=$(head -n 179 streets/buckingham_Place | tail -n 1 | cut -d '#' -f2)
+INTT=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -d '#' -f2)
 echo "$INTT"
 cat ./interviews/interview-"$INTT"
-echo $MAIN_SUSPECT
+echo "$MAIN_SUSPECT"
