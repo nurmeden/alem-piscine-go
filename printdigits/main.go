b78b41ab23bfb575e059b5dd62bcb11d0155a3fe
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-04 12:10:46 +0600

    printdigits

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-	var i int
-	for i = 0; i < 10; i++ {
-		fmt.Print(i)
+	for i := '0'; i < '9'; i++ {
+		z01.PrintRune(rune(i))
 	}
 	z01.PrintRune('\n')
 }
