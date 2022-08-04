2fd27e6ab927b8da4e14fbaac97ba1628d43d6fd
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-04 11:46:15 +0000

    Upload files to ''

---
+++ printcomb.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	start := '0'
+	end := '9'
+	for digit1 := start; digit1 <= end; digit1++ {
+		for digit2 := start; digit2 <= end; digit2++ {
+			if digit1 < digit2 {
+				for digit3 := start; digit3 <= end; digit3++ {
+					if digit2 < digit3 {
+						z01.PrintRune(digit1)
+						z01.PrintRune(digit2)
+						z01.PrintRune(digit3)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
