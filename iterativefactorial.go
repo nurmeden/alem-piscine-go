bdd29236145681daa6d45dc0691290b8b5947fc1
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-08 05:23:13 +0000

    Upload files to ''

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,10 +3,11 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb > 0 {
 		count := 1
-		for index := 1; index <= nb; index++ {
+		for index := 1; index < nb+1; index++ {
 			count *= index
 		}
 		return count
+	} else {
+		return 0
 	}
-	return 0
 }
