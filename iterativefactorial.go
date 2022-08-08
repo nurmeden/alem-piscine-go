a64e5105d74af940e5ca5f95b0419ba74b55aafe
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-08 05:29:26 +0000

    Upload files to ''

---
+++ iterativefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb >= 0 && nb < 21 {
+		count := 1
+		for index := 1; index < nb+1; index++ {
+			count *= index
+		}
+		return count
+	} else {
+		return 0
+	}
+}
