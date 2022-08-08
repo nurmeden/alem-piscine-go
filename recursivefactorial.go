70c01ba64ef3ba207f011adb8c5ff41e4160b24b
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-08 06:03:30 +0000

    Update 'recursivefactorial.go'

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,8 +1,8 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb >= 0 || nb < 21 {
-		if nb == 1 {
+	if nb >= 0 && nb < 21 {
+		if nb == 1 || nb == 0 {
 			return 1
 		}
 		if nb > 1 {
