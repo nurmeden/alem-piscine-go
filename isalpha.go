d289631138143614bd3f28b236a072fcd3821dc2
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-09 14:51:34 +0000

    Upload files to ''

---
+++ isalpha.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	s_rune := []rune(s)
+	count := 0
+	for _, elem := range s_rune {
+		if elem >= 'a' && elem <= 'z' || elem >= '0' && elem <= '9' || elem >= 'A' && elem <= 'Z' {
+			count++
+		}
+	}
+	return count == len(s)
+}
