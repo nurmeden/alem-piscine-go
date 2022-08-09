7f01d41d76cdb578e6abe5768f1f4f857f0d20a8
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-09 05:26:01 +0000

    Upload files to ''

---
+++ isprime.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb > 1 {
+		for elem := 2; elem < nb; elem++ {
+			if nb%elem == 0 {
+				return false
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
