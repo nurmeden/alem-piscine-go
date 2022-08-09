bf94e2a210d95c58762cb6dd65b22275b5387e5b
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-09 15:23:00 +0000

    Upload files to ''

--- isprintable.go
+++ isprintable.go
@@ -1,12 +1,11 @@
 package piscine
 
 func IsPrintable(s string) bool {
-	s_rune := []rune(s)
-	count := 0
-	for _, elem := range s_rune {
-		if elem >= 'a' && elem <= 'z' || elem >= '0' && elem <= '9' || elem >= 'A' && elem <= 'Z' {
-			count++
+	strs := []rune(s)
+	for i := range strs {
+		if strs[i] >= 0 && strs[i] <= 31 {
+			return false
 		}
 	}
-	return count == len(s)
+	return true
 }
