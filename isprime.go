16104eb00b71484fe2fb7772f46452b41d0b1734
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-09 05:52:04 +0000

    Upload files to ''

--- isprime.go
+++ isprime.go
@@ -1,14 +1,13 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb > 1 && nb < 100 {
+	if nb > 2 && nb < 100 {
 		for elem := 2; elem < nb; elem++ {
 			if nb%elem == 0 {
 				return false
-			} else {
-				return true
 			}
 		}
+		return true
 	}
 	if nb == 2 {
 		return true
