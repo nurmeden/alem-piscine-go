0aa4de9378845c39df368a999abff7206007b10e
Dulat_Nurmeden <dulat_nurmeden@noreply.example.org>
2022-08-09 07:58:40 +0000

    Upload files to ''

---
+++ findnextprime.go
@@ -0,0 +1,18 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb == 2 {
+		return nb
+	}
+	if nb > 0 {
+		for elem := 2; elem <= nb; elem++ {
+			if nb%elem == 0 {
+				nb = nb + 1
+				FindNextPrime(nb)
+			}
+		}
+		return nb
+	}
+	nb = 2
+	return FindNextPrime(nb)
+}
