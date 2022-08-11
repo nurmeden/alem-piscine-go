8790074cea18c7c4a3ea38b3a5e1b6ded2ab2f31
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-11 21:59:12 +0600

    main drthe fbdf

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -7,27 +7,33 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:]
-	caps := false
-	for _, v := range arguments {
-		if v == "--upper" {
-			caps = true
-		}
-	}
-	for _, arg := range arguments {
-		numv := 0
-		for _, v := range arg {
-			numv = numv*10 + int(v-'0')
-		}
-		if numv >= 1 && numv <= 26 {
-			if caps == false {
-				z01.PrintRune(rune(numv + 96))
-			} else {
-				z01.PrintRune(rune(numv + 64))
+	arguments := os.Args
+	car := false
+	for _, b := range arguments[1:] {
+		for _, v := range b {
+			if v != 0 {
+				car = true
 			}
-		} else {
-			z01.PrintRune(' ')
 		}
 	}
-	z01.PrintRune('\n')
+	if car {
+		for _, b := range arguments[1:] {
+			numv := 0
+			for _, v := range b {
+				numv = numv*10 + int(v-'0')
+			}
+			if numv >= 1 && numv <= 26 {
+				if arguments[1] == "--upper" {
+					z01.PrintRune(rune(numv + 64))
+				} else {
+					z01.PrintRune(rune(numv + 96))
+				}
+			} else if arguments[1] == "--upper" {
+				continue
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
 }
