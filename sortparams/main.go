d1595e515782bcffa4a38a16ad6944c493aa729b
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-11 11:02:33 +0600

    main5

---
+++ sortparams/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	for i := 0; i <= len(arguments)-1; i++ {
+		for j := 0; j < len(arguments)-1; j++ {
+			if arguments[i] < arguments[j] {
+				arguments[i], arguments[j] = arguments[j], arguments[i]
+			}
+		}
+	}
+	for i := 0; i <= len(arguments)-1; i++ {
+		for _, value := range arguments[i] {
+			z01.PrintRune(value)
+		}
+		z01.PrintRune('\n')
+	}
+}
