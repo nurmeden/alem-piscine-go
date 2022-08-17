27e09a4deb91fc63e68a4abdc45bb25ebb7928b1
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-17 16:29:37 +0600

    boolean main

---
+++ boolean/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	result := true
+	if nbr == 1 {
+		return result
+	} else {
+		result = false
+		return result
+	}
+}
+
+func main() {
+	arguments := os.Args[1:]
+	OddMsg := "I have an odd number of arguments"
+	EvenMsg := "I have an even number of arguments"
+	lengthOfArg := len(arguments)
+	if isEven(lengthOfArg) == true {
+		printStr(OddMsg)
+	} else {
+		printStr(EvenMsg)
+	}
+}
