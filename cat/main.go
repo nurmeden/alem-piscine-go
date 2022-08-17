02b679c17726735a6a9597ab4a9479dc320e9297
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-17 18:47:25 +0600

    cat

--- cat/main.go
+++ cat/main.go
@@ -1,73 +1,61 @@
-// package main
-
-// import (
-// 	"io/ioutil"
-// 	"os"
-
-// 	"github.com/01-edu/z01"
-// )
-
-// func main() {
-// 	file := os.Args
-
-// 	for i := 1; i < len(file)+1; i++ {
-// 		data, err := ioutil.ReadFile(file[i])
-// 		if file != nil {
-// 			error_msg := err.Error()
-// 			for _, val_err := range error_msg {
-// 				z01.PrintRune(val_err)
-// 			}
-// 			z01.PrintRune('\n')
-// 		} else {
-// 			for _, value := range data {
-// 				z01.PrintRune(rune(value))
-// 			}
-// 			z01.PrintRune('\n')
-// 		}
-// 	}
-// }
-
 package main
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
-	// "fmt"
 )
 
-func PrintResult(str string) {
-	for _, val := range str {
-		z01.PrintRune(val)
-	}
-}
-
-func MyReadFile(fileName string) string {
-	content, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return "error"
-	}
-	return string(content)
-}
-
 func main() {
-	args := os.Args[1:]
-	// abc := "1"
-	finish := false
-	for _, fileName := range args {
-		if _, err := os.Stat(fileName); err != nil {
-			PrintResult("open " + fileName + ": no such file or directory\n")
+	arguments := os.Args[1:]
+	length := 0
+	for l := range arguments {
+		length = l + 1
+	}
+
+	if length == 0 {
+		input, err := ioutil.ReadAll(os.Stdin)
+		for _, j := range string(input) {
+			z01.PrintRune(j)
+		}
+		if err != nil {
+			b := "ERROR: " + err.Error()
+			for _, e := range b {
+				z01.PrintRune(e)
+			}
+			z01.PrintRune('\n')
+			os.Exit(1)
+		}
+		return
+	}
+
+	for _, arg := range arguments {
+		file, err := os.Open(arg)
+		if err != nil {
+			b := "ERROR: " + err.Error()
+			for _, e := range b {
+				z01.PrintRune(e)
+			}
+			z01.PrintRune('\n')
+			os.Exit(1)
 			return
 		}
-		PrintResult(MyReadFile(fileName))
-		finish = true
-	}
-	if !finish {
-		reader := io.TeeReader(os.Stdin, os.Stdout)
-		ioutil.ReadAll(reader)
-		os.Stdin.Close()
-		os.Stdout.Close()
+
+		f, err := ioutil.ReadAll(file)
+
+		for _, text := range string(f) {
+			z01.PrintRune(text)
+		}
+		if err != nil {
+			b := "ERROR: " + err.Error()
+			for _, e := range b {
+				z01.PrintRune(e)
+			}
+			z01.PrintRune('\n')
+			os.Exit(1)
+		}
+
+		file.Close()
 	}
 }
