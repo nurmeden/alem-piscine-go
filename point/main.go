3033cd866eff937e9df65cd0112a999e133929b4
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-17 16:55:31 +0600

    sdgsd

--- point/main.go
+++ point/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
 
 type point struct {
-	x int
-	y int
+	x string
+	y string
 }
 
 func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+	ptr.x = "42"
+	ptr.y = "21"
 }
 
 func main() {
@@ -19,5 +19,21 @@ func main() {
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for _, value := range points.x {
+		z01.PrintRune(value)
+	}
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for _, value_y := range points.y {
+		z01.PrintRune(value_y)
+	}
+	z01.PrintRune('\n')
 }
