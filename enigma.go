2e6f4ec9e6d5eef50f64877cfccf9a1826ae249e
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-18 17:22:04 +0600

    hack5

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	a_e := ***a     // 5
+	b_e := *b       // 2
+	c_e := *******c // 7
+	d_e := ****d    // 6
+	***a = b_e
+	*b = d_e
+	*******c = a_e
+	****d = c_e
+}
