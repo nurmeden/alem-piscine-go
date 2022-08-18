e3ee765e5b0b14ea2c397d8cf04c3e8804da8659
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-18 12:36:21 +0600

    countif

--- countif.go
+++ countif.go
@@ -4,10 +4,10 @@ func CountIf(f func(string) bool, tab []string) int {
 	result_bool := false
 	count := 0
 	for _, value := range tab {
+		result_bool = f(string(value))
 		if result_bool == true {
 			count++
 		}
-		result_bool = f(string(value))
 	}
 	return count
 }
