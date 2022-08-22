6831ffe95b3f87af800dc9a31dea3ecb461c15a2
Your Name <you@example.com>
2022-08-22 19:02:01 +0600

    pushfront

---
+++ listpushfront.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	result := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = result
+		l.Tail = result
+	} else {
+		l.Tail.Next = result
+		l.Tail = result
+	}
+}
+
+func main() {
+	link := &List{}
+
+	ListPushFront(link, "Hello")
+	ListPushFront(link, "man")
+	ListPushFront(link, "how are you")
+
+	it := link.Head
+	for it != nil {
+		fmt.Print(it.Data, " ")
+		it = it.Next
+	}
+	fmt.Println()
+}
