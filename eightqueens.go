f6b0ea2b71fb95b45db077152baea0c376abd9d1
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-24 21:16:48 +0600

    eightqq

--- eightqueens.go
+++ eightqueens.go
@@ -4,90 +4,37 @@ import (
 	"github.com/01-edu/z01"
 )
 
-var (
-	list [100]int
-	kk   int
-)
-
-func EightQueens() {
-	for i := 0; i < 100; i++ {
-		list[i] = 0
-	}
-	kk = 0
-	var chessBoard [8][8]int
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			chessBoard[i][j] = 0
-		}
-	}
-	Solve8Q(chessBoard, 0)
-	for i := 0; i < 100; i++ {
-		for j := i; j < 100; j++ {
-			if list[i] > list[j] {
-				Swap(&list[i], &list[j])
-			}
-		}
-	}
-	for ii := 0; ii < 100; ii++ {
-		if list[ii] != 0 {
-			tmp := 10000000
-			runeNbr := list[ii]
-			for j := 0; j < 8; j++ {
-
-				sym := runeNbr / tmp
-				runeNbr = runeNbr - sym*tmp
-				tmp = tmp / 10
-				z01.PrintRune(rune(48 + sym))
-
-			}
-			z01.PrintRune('\n')
-		}
-	}
+func main() {
+	EightQueens()
 }
 
-func IsSafe(tableCheck [8][8]int, x int, y int) bool {
-	for i := 0; i < y; i++ {
-		if tableCheck[x][i] == 1 {
+func IsSafe(qn int, rp int, pos [8]int) bool {
+	for i := 0; i < qn; i++ {
+		t := pos[i]
+		if t == rp || t == rp-(qn-i) || t == rp+(qn-i) {
 			return false
-		}
-	}
-	for i := 0; i <= 7; i++ {
-		for j := 0; j <= 7; j++ {
-			if tableCheck[i][j] == 1 && (i-x == j-y || i-x == y-j) {
-				return false
-			}
 		}
 	}
 	return true
 }
 
-func Solve8Q(table [8][8]int, col int) bool {
-	if col == 8 {
-		PrintSeq(table)
-		return true
-	}
-	res := false
-	for i := 0; i < 8; i++ {
-		if IsSafe(table, i, col) {
-			table[i][col] = 1
-			res = Solve8Q(table, col+1)
-			table[i][col] = 0
+func solve(nb int, pos [8]int) {
+	if nb == 8 {
+		for i := 0; i < 8; i++ {
+			z01.PrintRune(rune(pos[i] + '1'))
 		}
-	}
-	return res
-}
-
-func PrintSeq(t [8][8]int) {
-	tmp := 10000000
-	k := 0
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			if t[i][j] == 1 {
-				k = k + (j+1)*tmp
-				tmp = tmp / 10
+		z01.PrintRune('\n')
+	} else {
+		for i := 0; i < 8; i++ {
+			if IsSafe(nb, i, pos) {
+				pos[nb] = i
+				solve(nb+1, pos)
 			}
 		}
 	}
-	kk++
-	list[kk] = k
+}
+
+func EightQueens() {
+	pos := [8]int{0, 0, 0, 0, 0, 0, 0}
+	solve(0, pos)
 }
