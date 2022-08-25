b9e16ba763f1fef2007cfef5a55938a18007b1c3
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-25 12:31:09 +0600

    erfb

--- quest12/btreesearchitem.go
+++ quest12/btreesearchitem.go
@@ -1,9 +1,9 @@
 package piscine
 
-// type TreeNode struct {
-// 	Left, Right, Parent *TreeNode
-// 	Data                string
-// }
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
@@ -18,17 +18,17 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	}
 }
 
-// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-// 	if root == nil {
-// 		return &TreeNode{Data: data}
-// 	}
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
 
-// 	if data < root.Data {
-// 		root.Left = BTreeInsertData(root.Left, data)
-// 		root.Left.Parent = root
-// 	} else if data > root.Data {
-// 		root.Right = BTreeInsertData(root.Right, data)
-// 		root.Right.Parent = root
-// 	}
-// 	return root
-// }
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else if data > root.Data {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+	return root
+}
