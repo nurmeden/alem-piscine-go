1f3d7350902d9c98f8f02062b1be85b06c9fcbf1
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-25 12:14:50 +0600

    btree

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -5,28 +5,71 @@ package piscine
 // 	Data                string
 // }
 
+func BTreeLevelCount(root *TreeNode) int {
+	if root.Right == nil && root.Left == nil {
+		return 1
+	}
+	if root.Right == nil && root.Left != nil {
+		return 1 + BTreeLevelCount(root.Left)
+	}
+	if root.Right != nil && root.Left == nil {
+		return 1 + BTreeLevelCount(root.Right)
+	}
+	if root.Right != nil && root.Left != nil {
+		return BTreeLevelCount(root.Left) + BTreeLevelCount(root.Right)
+	}
+	return 0
+}
+
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if elem < root.Data {
-		return BTreeSearchItem(root.Left, elem)
-	} else if elem > root.Data {
-		return BTreeSearchItem(root.Right, elem)
-	} else {
+	if root.Data == elem {
 		return root
 	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else {
+		return BTreeSearchItem(root.Right, elem)
+	}
 }
+
+// func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	f(root.Data)
+// 	BTreeApplyPreorder(root.Left, f)
+// 	BTreeApplyPreorder(root.Right, f)
+// }
+
+// func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	BTreeApplyPostorder(root.Left, f)
+// 	BTreeApplyPostorder(root.Right, f)
+// 	f(root.Data)
+// }
+
+// func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	BTreeApplyInorder(root.Left, f)
+// 	f(root.Data)
+// 	BTreeApplyInorder(root.Right, f)
+// }
 
 // func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 // 	if root == nil {
 // 		return &TreeNode{Data: data}
 // 	}
-
-// 	if data < root.Data {
+// 	if root.Data > data {
 // 		root.Left = BTreeInsertData(root.Left, data)
 // 		root.Left.Parent = root
-// 	} else if data > root.Data {
+// 	} else {
 // 		root.Right = BTreeInsertData(root.Right, data)
 // 		root.Right.Parent = root
 // 	}
