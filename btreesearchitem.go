2ec06d316c95a4ec98e386d9cb08c67dc57bd1be
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-25 12:25:01 +0600

    btree

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,5 +1,10 @@
 package piscine
 
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
 		return nil
@@ -11,4 +16,19 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	} else {
 		return root
 	}
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else if data > root.Data {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+	return root
 }
