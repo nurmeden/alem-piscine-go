bc183a3969d064a885b3a8e360a2d9ed1fffd6ec
Dulat_Nurmeden <nurmeden.02@gmail.com>
2022-08-26 19:00:27 +0600

    dcsv

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,34 +1,16 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if elem < root.Data {
-		return BTreeSearchItem(root.Left, elem)
-	} else if elem > root.Data {
-		return BTreeSearchItem(root.Right, elem)
-	} else {
+	if root.Data == elem {
 		return root
 	}
-}
-
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
+	if root.Data < elem {
+		return BTreeSearchItem(root.Right, elem)
+	} else {
+		return BTreeSearchItem(root.Left, elem)
 	}
-
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else if data > root.Data {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
-	}
-	return root
+	return nil
 }
