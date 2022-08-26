cb070513b2c6fb2879af4869cfdd9861f5dab022
Your Name <you@example.com>
2022-08-26 21:50:36 +0600

    fvedfw

---
+++ btreedeletenode.go
@@ -0,0 +1,45 @@
+package piscine
+
+// package main
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	piscine.BTreeInsertData(root, "5")
+// 	node := BTreeSearchItem(root, "4")
+// 	fmt.Println("Before delete:")
+// 	piscine.BTreeApplyInorder(root, fmt.Println)
+// 	root = BTreeDeleteNode(root, node)
+// 	fmt.Println("After delete:")
+// 	BTreeApplyInorder(root, fmt.Println)
+// }
+func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
+	if node == nil {
+		return root
+	}
+	if node.Data < root.Data {
+		root.Left = BTreeDeleteNode(root.Left, node)
+	} else if node.Data > root.Data {
+		root.Right = BTreeDeleteNode(root.Right, node)
+	} else {
+		if root.Left == nil {
+			temp := root.Right
+			root = nil
+			return temp
+		} else if root.Right == nil {
+			temp := root.Left
+			root = nil
+			return temp
+		} else {
+			temp := BTreeMin(root.Right)
+
+			root.Data = temp.Data
+			root.Right = BTreeDeleteNode(root.Right, temp)
+		}
+	}
+	return root
+}
