46be8554c2d3ce76a66ffc6d4f35c6bb2af5d4ba
TQ <tq@noreply.example.org>
2022-10-01 02:17:18 +0000

    Update 'doop/main.go'
    updated

--- doop/main.go
+++ doop/main.go
@@ -5,6 +5,43 @@ import (
 	"os"
 )
 
+func main() {
+	args := os.Args[1:]
+	num1 := 0
+	num2 := 0
+	u := false
+	o := false
+	if len(args) == 3 {
+		for _, arg1 := range args[0] {
+			if arg1 == 45 {
+				u = true
+				continue
+			}
+			if (arg1 < '0' || arg1 > '9') && arg1 != 45 {
+				return
+			}
+			num1 = (num1*10 + int((arg1 - 48)))
+		}
+		if u {
+			num1 *= -1
+		}
+		for _, arg2 := range args[2] {
+			if arg2 == 45 {
+				o = true
+				continue
+			}
+			if (arg2 < '0' || arg2 > '9') && arg2 != 45 {
+				return
+			}
+			num2 = (num2*10 + int((arg2 - 48)))
+		}
+		if o {
+			num2 *= -1
+		}
+		operations(num1, num2, args[1])
+	}
+}
+
 func operations(num1 int, num2 int, oper string) {
 	result := 0
 	if oper == "+" || oper == "\"+\"" {
@@ -44,41 +81,4 @@ func operations(num1 int, num2 int, oper string) {
 		result = num1 % num2
 		fmt.Println(result)
 	}
-}
-func main() {
-	args := os.Args[1:]
-	num1 := 0
-	num2 := 0
-	u := false
-	o := false
-	if len(args) == 3 {
-		for _, arg1 := range args[0] {
-			if arg1 == 45 {
-				u = true
-				continue
-			}
-			if (arg1 < '0' || arg1 > '9') && arg1 != 45 {
-				return
-			}
-			num1 = (num1*10 + int((arg1 - 48)))
-		}
-		if u {
-			num1 *= -1
-		}
-		for _, arg2 := range args[2] {
-			if arg2 == 45 {
-				o = true
-				continue
-			}
-			if (arg2 < '0' || arg2 > '9') && arg2 != 45 {
-				return
-			}
-			num2 = (num2*10 + int((arg2 - 48)))
-		}
-		if o {
-			num2 *= -1
-		}
-		operations(num1, num2, args[1])
-	}
-	return
 }
